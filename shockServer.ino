1439902f4993de07da58c2ebcadd26bee6ab9d8f
Ivan Bilych <ivanbilych@gmail.com>
2016-10-27 23:07:48 +0300

    Add new server features
    - confirmation for OK and restart commands
    - client disconnect
    - help message
    
    Signed-off-by: Ivan Bilych <ivanbilych@gmail.com>

--- shockServer.ino
+++ shockServer.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+#define ARRAY_SIZE(x) sizeof(x) / sizeof(x[0])
+
 #define DEBUG
 
 #ifdef DEBUG
@@ -20,6 +22,8 @@
 #define KEY_OK_CH       'o'
 #define RESET_SERVER_CH 'r'
 #define GET_STATS_CH    's'
+#define DISCONNECT_CH   'q'
+#define HELP_CH         'h'
 
 enum {
     KEY_MODE = 0,
@@ -27,10 +31,12 @@ enum {
 };
 
 int shockAttempts = 0;
-char passwordServer[] = { 'd', 'o', 'g' };
+
+char shockPhrase[] = { 's', 'h', 'o', 'c', 'k' };
+char restartPhrase[] = { 'r', 'e', 's', 't', 'a', 'r', 't' };
+
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte keyGPIO[] = { 2, 3 };
-byte serverResetCount = 0;
 
 IPAddress ip(192, 168, 0, 80);
 IPAddress mask(255, 255, 255, 0);
@@ -40,24 +46,14 @@ EthernetServer server(TELNET_PORT);
 EthernetClient client;
 
 boolean alreadyConnected = false;
-boolean handshakeDone = false;
 
-void(* resetFunc) (void) = 0;
+void (* resetFunc) (void) = 0;
 
 void setupSerial() {
     Serial.begin(SERIAL_SPEED);
     while ( !Serial ) { }
 
-    SERIAL_PRINT("Starting server on: ");
-    SERIAL_PRINT(Ethernet.localIP());
-    SERIAL_PRINT("\n");
-}
-
-void setupConnection() {
-    Ethernet.begin(mac, ip, serverDns, gateway, mask);
-    server.begin();
-
-    SERIAL_PRINT("Connection setup is complete\n");
+    SERIAL_PRINT("Serial setup is complete\n");
 }
 
 void setupGPIO() {
@@ -71,73 +67,74 @@ void setupGPIO() {
     SERIAL_PRINT("GPIO setup is complete\n");
 }
 
+void setupConnection() {
+    Ethernet.begin(mac, ip, serverDns, gateway, mask);
+    server.begin();
+
+    SERIAL_PRINT("Starting server on: ");
+    SERIAL_PRINT(Ethernet.localIP());
+    SERIAL_PRINT("\n");
+
+    SERIAL_PRINT("Connection setup is complete\n");
+}
+
 void setup() {
 #ifdef DEBUG
     setupSerial();
 #endif
-    setupConnection();
     setupGPIO();
+    setupConnection();
 }
 
-void handshake() {
-    int size = sizeof(passwordServer) / sizeof(passwordServer[0]);
-    char passwordUser[size];
+int confirmation(char* phrase, int size) {
+    char userPhrase[size];
 
-    PRINT("Password required\n");
-    PRINT(size);
-    PRINT(" symbols\n");
+    for ( ; !client.available(); ) {}
 
     for ( int i = 0; i < size; i++ ) {
-        passwordUser[i] = client.read();
+        userPhrase[i] = client.read();
     }
 
-    for ( int i = 0; i < size && passwordServer[i] == passwordUser[i]; i++ ) {
-        if ( passwordServer[i] != passwordUser[i] ) {
-            PRINT("Access denied\n");
+    for ( int i = 0; i < size; i++ ) {
+        if ( phrase[i] != userPhrase[i] ) {
+            PRINT("Wrong command\n");
 
-            return;
+            return 0;
         }
     }
 
-    PRINT("Access granted\n");
-
-    handshakeDone = true;
+    return 1;
 }
 
 void emulateKeypress(byte key) {
-    SERIAL_PRINT("Pressing key #");
-    SERIAL_PRINT(key);
-    SERIAL_PRINT("\n");
-
     digitalWrite(keyGPIO[key], LOW);
     delay(KEYPRESS_DELAY_MS);
     digitalWrite(keyGPIO[key], HIGH);
+    PRINT("Key ");
+    PRINT(key);
+    PRINT(" pressed\n");
 }
 
-void restartServer() {
-    if ( serverResetCount == 0 ) {
-        PRINT("WARNING! You are going to restart server\n");
-    } else if ( serverResetCount == 1 ) {
-        PRINT("WARNING! Server will be restarted on next attempt\n");
-    } else {
-        PRINT("RESTARTING SERVER IN 5 SEC\n");
-
-        delay(SERVER_RESTART_DELAY_MS);
-        resetFunc();
-    }
-
-    serverResetCount += 1;
-}
-
-void getStats() {
-    PRINT("Server statistics:\n");
-    PRINT("Number of shock attempts:");
+void getStats() {;
+    PRINT("Number of shock attempts: ");
     PRINT(shockAttempts);
     PRINT("\n");
 }
 
+void printHelp() {
+    PRINT("Server commands:\n");
+    PRINT("m - MODE button press\n");
+    PRINT("o - OK button press [confirmation required]\n");
+    PRINT("r - Restart server [confirmation required]\n");
+    PRINT("s - Show stats\n");
+    PRINT("q - Disconnect client\n");
+    PRINT("h - Print this help\n");
+}
+
 void proceedKeypress() {
     char command = client.read();
+    client.read();
+    client.read();
 
     switch ( command ) {
     case KEY_MODE_CH:
@@ -145,41 +142,57 @@ void proceedKeypress() {
         break;
 
     case KEY_OK_CH:
-        emulateKeypress(KEY_OK);
-        shockAttempts += 1;
+        PRINT("Enter 'shock' if you really want it\n");
+
+        if ( confirmation(shockPhrase, ARRAY_SIZE(shockPhrase)) ) {
+            emulateKeypress(KEY_OK);
+            shockAttempts += 1;
+        }
         break;
 
     case RESET_SERVER_CH:
-        restartServer();
+        PRINT("Enter 'restart' if you really want it\n");
+
+        if ( confirmation(restartPhrase, ARRAY_SIZE(restartPhrase)) ) {
+            PRINT("RESTARTING SERVER IN 5 SEC\n");
+
+            delay(SERVER_RESTART_DELAY_MS);
+
+            resetFunc();
+        }
         break;
 
     case GET_STATS_CH:
         getStats();
         break;
 
-    default:
-        PRINT("Unknown command\n");
+    case DISCONNECT_CH:
+        PRINT("Disconnecting client\n");
+
+        client.stop();
+        alreadyConnected = false;
+        break;
+
+    case HELP_CH:
+        printHelp();
+        break;
     }
 }
 
 void loop() {
     client = server.available();
 
-    if (client) {
+    if ( client ) {
         if ( !alreadyConnected ) {
             client.flush();
 
-            PRINT("Client connected to server\n");
+            printHelp();
 
             alreadyConnected = true;
         }
 
-        if ( client.available() && !handshakeDone ) {
-            handshake();
-        } else if ( client.available() && handshakeDone ) {
+        if ( client.available() ) {
             proceedKeypress();
         }
-    } else {
-        handshakeDone = false;
     }
 }
